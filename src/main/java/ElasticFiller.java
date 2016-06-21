382edaf40b36ca0b83947524ae0a5238d5041868
krummerd <krummerdmitriy@yahoo.com>
2016-06-22 00:00:51 +0300

    asda wewqewq2223

--- src/main/java/ElasticFiller.java
+++ src/main/java/ElasticFiller.java
@@ -1,30 +1,36 @@
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParser;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class ElasticFiller {
+public class ElasticFiller  {
 
     private List<elasticBean> objectListFromJson = new ArrayList<>();
 
-    JsonParser jParser = new JsonFactory();
-
-    try{
-
-
-
-    } catch (JsonGenerationException e) {
-        e.printStackTrace();
-    } catch (JsonMappingException e){
-        e.printStackTrace();
-    } catch (IOException e){
-        e.printStackTrace();
-    }
     public List<elasticBean> getObjectListFromJson() {
+        try{
+
+            JsonParser jParser = new JsonFactory().createParser(new File("elastic-data.json"));
+
+            elasticBean eBean = new elasticBean();
+
+
+
+
+
+
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
         return objectListFromJson;
     }
 
-    public void setObjectListFromJson(List<elasticBean> objectListFromJson) {
-        this.objectListFromJson = objectListFromJson;
-    }
 }
